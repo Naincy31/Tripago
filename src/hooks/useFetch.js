1cd29ca4d0be27f6752a6ab5e5b3ddfa10ae2d66
Naincy31 <iamnaincyrathore@gmail.com>
2024-01-21 00:25:38 +0530

    using hooks

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,25 @@
+import {useState, useEffect} from 'react'
+
+const useFetch = (url) => {
+    const [data, setData] = useState(null)
+    const [isPending, setIsPending] = useState(false)
+    const [error, setError] = useState(null)
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setIsPending(true)
+
+            const res = await fetch(url)
+            const json = await res.json()
+
+            setIsPending(false)
+            setData(json)
+        }
+
+        fetchData()
+    }, [url])
+
+    return {data, isPending}
+}
+
+export default useFetch;
